30b4eae9ec4386c3177cac61f64354caf0524ad3
yournameSyngasan <13715100034@163.com>
2019-08-17 21:17:18 +0800

    登录注册功能实现

--- src/views/Mine/center.vue
+++ src/views/Mine/center.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>个人中心</div>
+  <div>
+    <div>个人中心：</div>
+    <div>当前用户：{{$store.state.user.name}}</div>
+    <span @touchstart="handleToLogout" style="border:1px solid #ccc;fontSize:1rem">退出</span>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-name:"Center"
-}
+  name: "Center",
+  methods: {
+    handleToLogout() {
+      this.axios.get("/api2/users/logout").then(res => {
+        var status = res.data.status;
+        if (status === 0) {
+            this.$store.commit("user/USER_NAME",{name:''})
+          this.$router.push("/mine/login");
+        }
+      });
+    }
+  },
+  //   进入前先判断是否有登录
+  beforeRouteEnter: (to, from, next) => {
+    axios.get("/api2/users/getUser").then(res => {
+      if (res.data.status === 0) {
+        next(vm => {
+          vm.$store.commit("user/USER_NAME", { name: res.data.data.username });
+        });
+      } else {
+        next("/mine/login");
+      }
+    });
+  }
+};
 </script>
 
 <style>
-
 </style>
